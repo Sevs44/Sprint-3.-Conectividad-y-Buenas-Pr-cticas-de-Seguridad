41028fc5ddcf4190206618ee099846891622c171
Sevs44 <sebasbenenati@gmail.com>
2024-05-17 14:31:31 +0200

    First Commit from Android Studio

---
+++ app/src/test/java/com/example/sprint_3_conectividad_y_buenas_pr_cticas_de_seguridad/ExampleUnitTest.kt
@@ -0,0 +1,16 @@
+package com.example.sprint_3_conectividad_y_buenas_pr_cticas_de_seguridad
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
